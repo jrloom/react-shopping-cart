ce7356cdfdc7abeb19bb9abc1b7a19660beee543
Jason Loomis <jason@jrloomis.com>
2019-09-18 21:34:40 -0400

    step 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -20,14 +21,20 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={{ cart }}>
+          <Navigation cart={cart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
       {/* Routes */}
       <ProductContext.Provider value={{ products, addItem }}>
-        <Route exact path="/" component={Products} />
+        <CartContext.Provider value={{ cart }}>
+          <Route exact path="/" component={Products} />
+        </CartContext.Provider>
       </ProductContext.Provider>
       <ProductContext.Provider value={{ products, addItem }}>
-        <Route path="/cart" component={ShoppingCart} />} />
+        <CartContext.Provider value={{ cart }}>
+          <Route path="/cart" component={ShoppingCart} />} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
